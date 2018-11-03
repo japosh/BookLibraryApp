3515faf9c1e77aa089340514056fabc2a47653bc
Lucas Oshiro <lucas.oshiro@hotmail.com>
2018-11-02 22:09:02 -0300

    First commit.

---
+++ app/src/main/java/br/unicid/livraria/Inicio.java
@@ -0,0 +1,42 @@
+package br.unicid.livraria;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inicio);
+    }
+
+    public void catalogo(View botao){
+        Intent it = new Intent(this, Catalogo.class);
+        startActivity(it);
+
+
+    }
+
+    public void dados(View botao){
+        Intent it = new Intent(this, AdmDados.class);
+        startActivity(it);
+
+
+    }
+
+    public void equipe(View botao){
+        Intent it = new Intent(this, Equipe.class);
+        startActivity(it);
+
+
+    }
+
+    public void fechar(View botao){
+        finishAffinity();
+        finish();
+
+    }
+}
