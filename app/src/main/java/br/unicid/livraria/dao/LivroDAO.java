94cae064681dfade614fae3f6e0c2726c35d306d
Tiago <tml0289@gmail.com>
2018-11-13 21:18:06 -0200

    Classe Livro pronta

--- app/src/main/java/br/unicid/livraria/dao/LivroDAO.java
+++ app/src/main/java/br/unicid/livraria/dao/LivroDAO.java
@@ -9,6 +9,104 @@ import br.unicid.livraria.util.DatabaseFactory;
 
 public class LivroDAO {
 
+    private Context context;
+
+    public LivroDAO(Context context) {
+        this.context = context;
+    }
 
 
+    public void create(Aluno aluno) {
+        //Obtendo o objeto que representa o banco de dados
+        SQLiteDatabase db = DatabaseFactory.getDatabase(context);
+
+        //Preparando e executando o comando SQL
+        String sql = "INSERT INTO aluno VALUES (?, ?, ?)";
+        String params[] = {aluno.getRgm(), aluno.getNome(), aluno.getEmail()};
+        db.execSQL(sql, params);
+
+        //Fechando o banco de dados
+        db.close();
+    }
+
+    public void update(Aluno aluno) {
+        //Obtendo o objeto que representa o banco de dados
+        SQLiteDatabase db = DatabaseFactory.getDatabase(context);
+
+        //Preparando e executando o comando SQL
+        String sql = "UPDATE aluno SET nome = ?, email = ? WHERE rgm = ?";
+        String params[] = {aluno.getNome(), aluno.getEmail(), aluno.getRgm()};
+        db.execSQL(sql, params);
+
+        //Fechando o banco de dados
+        db.close();
+    }
+
+    public void delete(String rgm) {
+        //Obtendo o objeto que representa o banco de dados
+        SQLiteDatabase db = DatabaseFactory.getDatabase(context);
+
+        //Preparando e executando o comando SQL
+        String sql = "DELETE FROM aluno WHERE rgm = ?";
+        String params[] = {rgm};
+        db.execSQL(sql, params);
+
+        //Fechando o banco de dados
+        db.close();
+    }
+
+    public Aluno findByRgm(String rgm) {
+        //Obtendo o objeto que representa o banco de dados
+        SQLiteDatabase db = DatabaseFactory.getDatabase(context);
+
+        //Preparando e executando o comando SQL e obtendo os dados
+        String sql = "SELECT * FROM aluno WHERE rgm = ?";
+        String params[] = {rgm};
+        Cursor rs = db.rawQuery(sql, params);
+
+        Aluno aluno = null;
+
+        if (rs.moveToNext()) {
+            aluno = new Aluno();
+            aluno.setRgm(rs.getString(0)); //campo rgm
+            aluno.setNome(rs.getString(1)); //campo nome
+            aluno.setEmail(rs.getString(2)); //campo email
+        }
+
+        //Fechando o banco de dados
+        db.close();
+
+        //Retornando o objeto aluno
+        return aluno;
+    }
+
+    public List<Aluno> findAll() {
+        //Obtendo o objeto que representa o banco de dados
+        SQLiteDatabase db = DatabaseFactory.getDatabase(context);
+
+        //Preparando e executando o comando SQL e obtendo os dados
+        String sql = "SELECT * FROM aluno";
+        Cursor rs = db.rawQuery(sql, null);
+
+        //Criando um objeto Lista de alunos (vazia)
+        List<Aluno> alunos = new ArrayList<>();
+
+        //Enquanto houver linhas registros...
+        while (rs.moveToNext()) {
+            //Cria um objeto aluno, preencha com os dados da linha
+            Aluno aluno = new Aluno();
+            aluno.setRgm(rs.getString(0)); //campo rgm
+            aluno.setNome(rs.getString(1)); //campo nome
+            aluno.setEmail(rs.getString(2)); //campo email
+
+            //Adiciona o objeto à lista
+            alunos.add(aluno);
+        }
+
+        //Fechando o banco de dados
+        db.close();
+
+        //Retornando o objeto aluno
+        return alunos;
+    }
 }
--- app/src/main/java/br/unicid/livraria/domain/Livro.java
+++ app/src/main/java/br/unicid/livraria/domain/Livro.java
@@ -2,7 +2,89 @@ package br.unicid.livraria.domain;
 
 public class Livro {
 
+    private String titulo;
+    private String subTitulo;
+    private String edicao;
+    private String autor;
+    private String quantPag;
+    private String anoPub;
+    private String editora;
+
+    public Livro() {
+
+    }
 
     public Livro(String isbn, String titulo, String subTitulo, String edicao, String autor, String quantPag, String anoPub, String editora) {
+
     }
+
+    private String isbn;
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getSubTitulo() {
+        return subTitulo;
+    }
+
+    public void setSubTitulo(String subTitulo) {
+        this.subTitulo = subTitulo;
+    }
+
+    public String getEdicao() {
+        return edicao;
+    }
+
+    public void setEdicao(String edicao) {
+        this.edicao = edicao;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getQuantPag() {
+        return quantPag;
+    }
+
+    public void setQuantPag(String quantPag) {
+        this.quantPag = quantPag;
+    }
+
+    public String getAnoPub() {
+        return anoPub;
+    }
+
+    public void setAnoPub(String anoPub) {
+        this.anoPub = anoPub;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+
+
+
 }
