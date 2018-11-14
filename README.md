b7d03d08899a152bac729be354a2789fecb41e86
Tiago <tml0289@gmail.com>
2018-11-14 20:26:22 -0200

    Falta colocar as imagens e mostrar a lista de livros

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # BookLibraryApp
 Android application for a Book Library usage.
+
+#Tiago
+- Ainda estou vendo como colocar as imagens dos livros.
+- Falta colocar a listar de livros
+- Verificar por bugs.
--- app/src/main/java/br/unicid/livraria/InfoLivro.java
+++ app/src/main/java/br/unicid/livraria/InfoLivro.java
@@ -1,15 +1,62 @@
 package br.unicid.livraria;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import br.unicid.livraria.dao.LivroDAO;
+import br.unicid.livraria.domain.Livro;
 
 public class InfoLivro extends AppCompatActivity {
+
+    private TextView txtISBN;
+    private TextView txtTitulo;
+    private TextView txtSubTitulo;
+    private TextView txtEdicao;
+    private TextView txtAutor;
+    private TextView txtQuantPag;
+    private TextView txtAnoPub;
+    private TextView txtEditora;
+
+    private String isbn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info_livro);
+
+        txtISBN = (TextView) findViewById(R.id.txtISBN);
+        txtTitulo = (TextView) findViewById(R.id.txtTitulo);
+        txtSubTitulo  = (TextView) findViewById(R.id.txtSubTitulo);
+        txtEdicao = (TextView) findViewById(R.id.txtEdicao);
+        txtAutor = (TextView) findViewById(R.id.txtAutor);
+        txtQuantPag = (TextView) findViewById(R.id.txtQuantPag);
+        txtAnoPub = (TextView) findViewById(R.id.txtAnoPub);
+        txtEditora = (TextView) findViewById(R.id.txtEditora);
+
+        //Recebe o isbn do livro da página catalogo
+        Intent getIsbn = getIntent();
+        isbn  = getIsbn.getStringExtra("isbn");
+
+        //Pesquisa o libro no banco
+        Livro livro = new LivroDAO(this).findByIsbn(isbn);
+
+        if (livro != null){
+
+            //Preenche os campos com as informações do livro
+            txtAnoPub.setText(livro.getAnoPub());
+            txtAutor.setText(livro.getAutor());
+            txtEdicao.setText(livro.getEdicao());
+            txtEditora.setText(livro.getEditora());
+            txtISBN.setText(livro.getIsbn());
+            txtQuantPag.setText(livro.getQuantPag());
+            txtSubTitulo.setText(livro.getSubTitulo());
+            txtTitulo.setText(livro.getTitulo());
+        }
+
     }
 
     public void voltar(View botao){
