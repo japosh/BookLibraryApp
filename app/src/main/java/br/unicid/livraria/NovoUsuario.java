e54fd29623fbbdc6e3cc7ff1d17c1104718703d0
japosh <lucas.oshiro@hotmail.com>
2018-11-21 02:28:11 -0200

    [fix] - new user password confirmation check

--- app/src/main/java/br/unicid/livraria/NovoUsuario.java
+++ app/src/main/java/br/unicid/livraria/NovoUsuario.java
@@ -13,6 +13,7 @@ public class NovoUsuario extends AppCompatActivity {
 
     private EditText txtUser;
     private EditText txtPass;
+    private EditText txtPassNew;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,26 +22,38 @@ public class NovoUsuario extends AppCompatActivity {
 
         txtUser = (EditText) findViewById(R.id.txtUser);
         txtPass = (EditText) findViewById(R.id.txtPass);
+        txtPassNew = (EditText) findViewById(R.id.txtPassNew);
     }
 
-    public void salvar(View view) {
+    public void salvar(View view){
         //Obtendo os valores dos campos
         String user = txtUser.getText().toString();
         String pass = txtPass.getText().toString();
+        String passCheck = txtPassNew.getText().toString();
 
-        //Criando um objeto aluno com os dados dos campos
-        Usuario usuario = new Usuario(user, pass);
+        if( passCheck.equals( pass ) ) {
+            //Criando um objeto aluno com os dados dos campos
+            Usuario usuario = new Usuario(user, pass);
 
-        //DAO para inserção do registro
-        UsuarioDAO dao = new UsuarioDAO(this);
-        dao.create(usuario);
+            //DAO para inserção do registro
+            UsuarioDAO dao = new UsuarioDAO(this);
+            dao.create(usuario);
 
-        //Exibe um alerta de sucesso.
-        Toast.makeText(this, "Usuário cadastrado com sucesso", Toast.LENGTH_SHORT).show();
+            //Exibe um alerta de sucesso.
+            Toast.makeText(this, "Usuário cadastrado com sucesso", Toast.LENGTH_SHORT).show();
 
-        //Limpando os campos
-        txtUser.setText(null);
-        txtPass.setText(null);
+            //Limpando os campos
+            txtUser.setText(null);
+            txtPass.setText(null);
+            txtPassNew.setText(null);
+
+        }else{
+            Toast.makeText(this, "As novas senhas não batem!", Toast.LENGTH_SHORT).show();
+
+            //Limpando os campos
+            txtPass.setText(null);
+            txtPassNew.setText(null);
+        }
     }
 
     public void voltar(View botao){
